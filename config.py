49f00fd3a5bd19c66dbbc253bfbda706c821b202
o-keita <o.keita@alustudent.com>
2025-03-07 09:29:32 +0000

    added: folder ID

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'your-secret-key')
 CLIENT_SECRET_FILE = os.environ.get('CLIENT_SECRET_FILE', 'credentials.json')
 CLIENT_ID = os.environ.get('CLIENT_ID', '1014200638385-dd0slrn5cj29dgurrkpek99und20sbhg.apps.googleusercontent.com')
 SCOPES = ['openid', 'https://www.googleapis.com/auth/drive']
-DRIVE_FOLDER_ID = os.environ.get('DRIVE_FOLDER_ID', 'your-marketing-folder-id')
+DRIVE_FOLDER_ID = os.environ.get('DRIVE_FOLDER_ID', '14fMC56054LgVTRm1wzSg2f3pPCFjZW-n')
