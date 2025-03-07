de9f253f090d8efd49849b7a95229b8e9d909d8e
o-keita <o.keita@alustudent.com>
2025-03-07 08:47:52 +0000

    added: home route

--- app.py
+++ app.py
@@ -6,6 +6,12 @@ from config import SECRET_KEY
 app = Flask(__name__)
 app.secret_key = SECRET_KEY
 
+
+@app.route('/')
+def home():
+    return "Welcome to the Google Drive API!"
+
+
 # Routes for authentication and listing files
 app.add_url_rule('/authorize', view_func=authorize)
 app.add_url_rule('/oauth2callback', view_func=oauth2callback)
