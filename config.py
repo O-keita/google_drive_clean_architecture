502e3241a4a843f8730557dc478855d3e442e23f
o-keita <o.keita@alustudent.com>
2025-03-07 09:22:04 +0000

    added: authorize button

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 
 SECRET_KEY = os.environ.get('SECRET_KEY', 'your-secret-key')
 CLIENT_SECRET_FILE = os.environ.get('CLIENT_SECRET_FILE', 'credentials.json')
-CLIENT_ID = os.environ.get('CLIENT_ID', '199828944153-cg1hmm5jtdg647ogcs4ag3nfkj6e05qh.apps.googleusercontent.com')
+CLIENT_ID = os.environ.get('CLIENT_ID', '1014200638385-dd0slrn5cj29dgurrkpek99und20sbhg.apps.googleusercontent.com')
 SCOPES = ['openid', 'https://www.googleapis.com/auth/drive']
 DRIVE_FOLDER_ID = os.environ.get('DRIVE_FOLDER_ID', 'your-marketing-folder-id')
